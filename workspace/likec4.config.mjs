c242769c9eb6f5486fd18542faef214ad7345f5b
Adolfo Baeza <adolfo.baeza@gmail.com>
2026-02-23 11:20:14 -0300

    Organizacion de aplicaciones que usan capacidades y capacidades que soportan a cada aplicacion

--- workspace/likec4.config.mjs
+++ workspace/likec4.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'likec4/config'
-import generators from './likec4-global'
+// import generators from './likec4-global'
 
 export default defineConfig({
   name: 'essential-model',
   title: 'Vista unificada de sistemas',
-  entry: "systems.essential.c4",
+  // entry: "systems.essential.c4",
   generators,
   include: [
     "./capability.essential.c4",
