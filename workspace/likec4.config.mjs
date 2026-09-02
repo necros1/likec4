a3d0c99529aba6af6301cf21c4c75211546b55cc
Adolfo Baeza <adolfo.baeza@gmail.com>
2026-09-02 00:02:13 -0400

    Reparacion C4 Model

--- workspace/likec4.config.mjs
+++ workspace/likec4.config.mjs
@@ -6,11 +6,15 @@ export default defineConfig({
   title: 'Vista unificada de sistemas',
   // entry: "systems.essential.c4",
   generators,
-  include: [
-    "./capability.essential.c4",
-    "./systems.essential.c4",
-    "./specification.global.c4",
-  ],
+  "include": {
+    "paths": [
+        "./shared/specification.deployment.c4",
+      "./shared/specification.element.c4",
+      "./shared/specification.relationship.c4",
+      "./shared/specification.tag.fai.c4",
+      "./shared/systems.external.c4",
+    ]
+  },
   styles: {
     defaults: {
       opacity: 10,
